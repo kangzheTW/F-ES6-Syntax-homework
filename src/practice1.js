8d781fca6774f1538c9b40868c474dace4d16f49
Zhe Kang <zkang@thoughtworks.com>
2020-07-02 22:30:29 +0800

    finish practice 1,2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,8 @@
-const parseData
+const parseData = (response) => {
+    return response.data.map(user => {
+        return user.reduce((acc, cur, index) => {
+            return Object.assign(acc, {[response.column[index].name]: cur});
+        }, {})
+    })
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,10 @@
-const inject
+const inject = (items, sections) => {
+  const result = [...items];
+  sections
+    .sort((s1, s2) => s2.index - s1.index)
+    .forEach((section) => {
+      result.splice(section.index, 0, section.content);
+    });
+  return result;
+};
 export { inject };
